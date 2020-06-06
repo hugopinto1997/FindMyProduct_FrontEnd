dc9b27e9b664fe8ecba3e7bfd18f81cb4c566780
hugopinto1997 <hugopillo@hotmail.com>
2020-06-06 04:07:40 -0600

    user page design

--- lib/src/pages/tabs/tab_user_page.dart
+++ lib/src/pages/tabs/tab_user_page.dart
@@ -23,9 +23,127 @@ class _TabUserPageState extends State<TabUserPage> {
           }, alignment: Alignment.centerLeft,)
         ],
       ),
-      body: Center(child: Text('User Page'),),
+      body: Stack(
+        children: <Widget>[
+          _crearFondo(context),
+          
+        ],
+      ),
     );
   }
+
+
+Widget _crearFondo(BuildContext context){
+  final size = MediaQuery.of(context).size;
+
+  final fondoSuperior = Container(
+    height: size.height*0.2,
+    width: double.infinity,
+    child: FadeInImage(
+            fit: BoxFit.cover,
+            placeholder: AssetImage('assets/no-image.jpg'),
+            image: NetworkImage('https://images.theconversation.com/files/223713/original/file-20180619-38837-1t04rux.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=496&fit=clip')),
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        colors: <Color>[
+          Color.fromRGBO(6, 66, 186, 1.0),
+          Color.fromRGBO(56, 114, 232, 1.0),
+        ],
+        ),
+    ),
+  );
+
+
+    return Stack(
+      children: <Widget>[
+        fondoSuperior,
+
+        Container(
+          padding: EdgeInsets.only(top: 65,),
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(backgroundColor: Color.fromRGBO(158, 27, 27, 1), child: Text('H',style: TextStyle(fontSize: 75),),radius: 80,
+              foregroundColor: Colors.white),
+              SizedBox(height: 20, width: double.infinity,),
+              Text('Nombre de Usuario', style: Theme.of(context).textTheme.bodyText1.copyWith(fontSize: 28),),
+              
+              SizedBox(height: 28,),
+
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(width: size.width*0.8, height: 36, color: Colors.blueGrey,
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                  Icon(Icons.shopping_cart, color: Colors.white,),
+                  SizedBox(width: 20,),
+                  Text('Tienes 2 listas disponibles', style: Theme.of(context).textTheme.subtitle1.copyWith(color: Colors.white),)
+                ],),
+                )),
+
+              SizedBox(height: 20,),  
+            
+              Expanded(
+                  child: Container(
+                    width: size.width*0.8,
+                   // color: Colors.white,
+                    child: Column(children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: <Widget>[
+                        Icon(Icons.mail,size: 28, color: Colors.lightBlue,),
+                        SizedBox(width: 20,),
+                        Text('example@example.com', style: Theme.of(context).textTheme.subtitle1,)
+                      ],),
+
+                      SizedBox(height: 10,),
+
+                       Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                        Icon(Icons.phone_android,size: 28, color: Colors.lightBlue,),
+                        SizedBox(width: 20,),
+                        Text('77448899', style: Theme.of(context).textTheme.subtitle1,)
+                      ],),
+
+                       SizedBox(height: 10,),
+
+                       Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                        Icon(Icons.people,size: 28, color: Colors.lightBlue,),
+                        SizedBox(width: 20,),
+                        Text('Tienes 24 amigos', style: Theme.of(context).textTheme.subtitle1,)
+                      ],),
+
+                       SizedBox(height: 10,),
+
+                       Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                        Icon(Icons.people_outline,size: 28, color: Colors.lightBlue,),
+                        SizedBox(width: 20,),
+                        Text('Tienes 2 solicitudes de amistad', style: Theme.of(context).textTheme.subtitle1,)
+                      ],),
+                    ],),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+
+}
+
+
+
 
   void settingsModal(BuildContext context){
   showDialog(
