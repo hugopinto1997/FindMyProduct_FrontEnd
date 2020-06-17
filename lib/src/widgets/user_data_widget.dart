54d7b0cdb5c0445756b60e6cf7b0f32e4bfa1fb1
hugopinto1997 <hugopillo@hotmail.com>
2020-06-17 04:29:52 -0600

    user page bug fixes

--- lib/src/widgets/user_data_widget.dart
+++ lib/src/widgets/user_data_widget.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 Widget userData(BuildContext context, IconData icono, String text){
+  final size = MediaQuery.of(context).size;
   return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
       children: <Widget>[
       Icon(icono,size: 28, color: Colors.lightBlue,),
       SizedBox(width: 20,),
-      Text(text, style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis, maxLines: 2)
+      Container(
+        width: size.width*0.5,
+        child: Text(text, style: Theme.of(context).textTheme.subtitle1, overflow: TextOverflow.ellipsis, maxLines: 2,))
     ],
   );
 }
