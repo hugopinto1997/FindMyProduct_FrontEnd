1a11cd94a8f27676eb962a699433902c035cbd39
hugopinto1997 <hugopillo@hotmail.com>
2020-07-05 23:19:35 -0600

    edit list fixed

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,5 +1,16 @@
 
 
+/*
+ * FindMyProduct app
+ * Copyright (C) 2020 hugopinto1997
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>
+ */
+
 import 'package:prototipo_super_v2/src/bloc/validatos.dart';
 import 'package:rxdart/rxdart.dart';
 
--- lib/src/providers/lists_action_cable_provider.dart
+++ lib/src/providers/lists_action_cable_provider.dart
@@ -1,3 +1,14 @@
+/*
+ * FindMyProduct app
+ * Copyright (C) 2020 hugopinto1997
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>
+ */
+
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
@@ -91,7 +102,7 @@ class ListsActionCableProvider with ChangeNotifier {
      // initCable();
       final direccion = Uri.https(
       'findmyproduct-api.herokuapp.com',
-      'api/v1/lists/update.json', 
+      'api/v1/lists/$id', 
       {
         'list[name]': name
       }
